1cd51f370ee8673ddc488df01f70b1ccdc487af0
mkokor <matijakokorr@gmail.com>
2023-06-09 19:24:46 +0200

    task 1 solution added

---
+++ pyton-scripts/task-1/task-1.py
@@ -0,0 +1,17 @@
+# Calculating some mathematical expressions...
+
+
+import numpy as np
+
+
+a = 13 * 2.5 ** (124 / 9.3) + 3 ** (1 / 2)
+print(f"RESULT a: {a}")
+
+b = np.cos(np.pi / 3) + np.tan(np.pi * np.exp(np.pi))
+print(f"RESULT b: {b}")
+
+c = (2 + 3 ** 8) * 10
+print(f"RESULT c: {c}")
+
+d = 79 // 18
+print(f"RESULT d: {d}")
